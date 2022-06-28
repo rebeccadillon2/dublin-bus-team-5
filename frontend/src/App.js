b3bbede9cdb2b20e290027f3019a3e4b1f760816
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-06-28 15:41:21 +0100

    Remove unused files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
