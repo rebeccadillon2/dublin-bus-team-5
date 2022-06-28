fccac1318d54bde09080fe92eb9eaa203a09fe6f
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-06-28 16:00:11 +0100

    Add tailwind and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,22 @@
-import React, { useEffect } from "react";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./styles/globals.css";
+import "tailwindcss/tailwind.css";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-  useEffect(() => {
-    console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
-  }, []);
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/signup"} element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
