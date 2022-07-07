5d8583fa849a92e8249aaca4bd6317e81a32a150
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-07 13:26:10 +0100

    Create getStopTimes function

--- frontend/src/lib/api/bus.js
+++ frontend/src/lib/api/bus.js
@@ -1,1 +1,11 @@
-export {};
+import axios from "axios";
+import { baseUrl } from ".";
+
+export function getStopTimes(stopId, time) {
+  return axios.get(`${baseUrl}/api/bus/get-stop-times/`, {
+    params: {
+      stopId,
+      time,
+    },
+  });
+}
