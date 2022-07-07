271bc45cddc1eba2402bd6271a8b8877691ba388
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-07 22:41:12 +0100

    Create stops and routes requests

--- frontend/src/lib/api/bus.js
+++ frontend/src/lib/api/bus.js
@@ -9,3 +9,28 @@ export function getStopTimes(stopId, time) {
     },
   });
 }
+
+export function getShortRouteNames(stopTimeObj) {
+  return axios.get(`${baseUrl}/api/bus/get-route-names/`, {
+    params: {
+      stopTimeObj,
+      is_private: false,
+    },
+  });
+}
+
+export function getShortRouteName(tripId1, tripId2, tripId3, tripId4, tripId5) {
+  return axios.get(`${baseUrl}/api/bus/get-route-name/`, {
+    params: {
+      tripId1,
+      tripId2,
+      tripId3,
+      tripId4,
+      tripId5,
+    },
+  });
+}
+
+export function getAllStops() {
+  return axios.get(`${baseUrl}/api/bus/get-all-stops/`);
+}
--- frontend/src/pages/testing.js
+++ frontend/src/pages/testing.js
@@ -1,15 +1,65 @@
-import React from "react";
-import { getStopTimes } from "../lib/api";
+import React, { useEffect, useState } from "react";
+import { getAllStops, getShortRouteName, getStopTimes } from "../lib/api";
 
 export function Testing() {
+  const [stopData, setStopData] = useState(null);
+  const [error, setError] = useState(false);
   const handleClick = async () => {
     try {
-      const data = await getStopTimes("8240DB000226", "07:00:00");
-      console.log("DATA", data);
+      const { data } = await getStopTimes("8240DB000226", "07:00:00");
+      const res = await getShortRouteName(
+        data[0].tripId,
+        data[1].tripId,
+        data[2].tripId,
+        data[3].tripId,
+        data[4].tripId
+      );
+
+      const newState = data.map((obj) => {
+        if (obj.tripId === res.data[0].tripId) {
+          return { ...obj, routeShortName: res.data[0].routeShortName };
+        }
+        if (obj.tripId === res.data[1].tripId) {
+          return { ...obj, routeShortName: res.data[1].routeShortName };
+        }
+        if (obj.tripId === res.data[2].tripId) {
+          return {
+            ...obj,
+            routeShortName: res.data[2].routeShortName,
+          };
+        }
+        if (obj.tripId === res.data[3].tripId) {
+          return {
+            ...obj,
+            routeShortName: res.data[3].routeShortName,
+          };
+        }
+        if (obj.tripId === res.data[4].tripId) {
+          return {
+            ...obj,
+            routeShortName: res.data[4].routeShortName,
+          };
+        }
+        return { ...obj, routeShortName: res.data[0].routeShortName };
+      });
+      setStopData(newState);
     } catch (e) {
       console.log(e);
+      setError(true);
     }
   };
+
+  useEffect(() => {
+    const getAllStopsData = async () => {
+      try {
+        const { data } = await getAllStops();
+        console.log("ALl stops: ", data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getAllStopsData();
+  }, []);
 
   return (
     <>
@@ -40,6 +90,25 @@ export function Testing() {
         >
           Get all Stop Times
         </button>
+      </div>
+      <div className='flex pl-4'>
+        {error ? (
+          <div>Error</div>
+        ) : (
+          <div>
+            {stopData &&
+              stopData[0].routeShortName &&
+              stopData
+                .sort((a, b) => {
+                  return a.arrivalTime - b.arrivalTime;
+                })
+                .map((item) => (
+                  <p key={item.id}>
+                    {item.routeShortName} - {item.arrivalTime}
+                  </p>
+                ))}
+          </div>
+        )}
       </div>
     </>
   );
