4b063d352333cc30d030f516554fb887111e67ff
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-19 15:16:34 +0100

    Creating the Spotify urls

---
+++ dublin_bus/spotify/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('redirect/', spotify_callback),
+    path('get-auth-url/', AuthUrl.as_view()),
+    path('play-track/', PlayTrack.as_view()),
+    path('pause-track/', PauseTrack.as_view()),
+    path('current-song/', CurrentSong.as_view()),
+    path('get-access-token/', GetAccessToken.as_view()),
+    path('get-podcasts/', GetDublinPodcasts.as_view()),
+    path('is-authenticated/', IsAuthenticated.as_view()),
+    path('get-podcast-episodes/', GetDublinPodcastEpisodes.as_view()),
+]
