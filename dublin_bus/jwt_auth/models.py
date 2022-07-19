54457e29fa9b9e196b53d3a506cfab446651c5c1
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-19 13:11:58 +0100

    Creating user model

--- dublin_bus/jwt_auth/models.py
+++ dublin_bus/jwt_auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=50, unique=True)
+    profile_image = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f'{self.email}'
