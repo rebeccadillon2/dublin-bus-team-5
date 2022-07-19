2475f374198ababc04c427ddf9201b08bf3135bf
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-19 23:45:15 +0100

    Rename files

--- frontend/src/components/account/index.js
+++ frontend/src/components/account/index.js
@@ -1,5 +1,5 @@
-export * from "./danger";
-export * from "./general";
+export * from "./Danger";
+export * from "./General";
 export * from "./Profile";
 export * from "./edit-email";
 export * from "./edit-image";
