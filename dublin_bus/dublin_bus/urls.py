5dc2c614274a9a1384724d975c87463fbd4d081f
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-19 13:55:55 +0100

    Add api urls to project urls

--- dublin_bus/dublin_bus/urls.py
+++ dublin_bus/dublin_bus/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/bus/', include('api.urls')),
     path('api/auth/', include('jwt_auth.urls')),
 ]
