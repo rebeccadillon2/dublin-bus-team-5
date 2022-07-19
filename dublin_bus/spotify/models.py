ec166b08ff872691de731cda8636b5912146c773
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-19 14:59:01 +0100

    Create Spotify Token model

--- dublin_bus/spotify/models.py
+++ dublin_bus/spotify/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Spotify Token Model
+class SpotifyToken(models.Model):
+    user = models.IntegerField(unique=True)
+    access_token = models.CharField(max_length=300)
+    refresh_token = models.CharField(max_length=300)
+    expires_in = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    token_type = models.CharField(max_length=100)
