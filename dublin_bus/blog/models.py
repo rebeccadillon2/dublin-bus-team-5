3dae090cbf615e26ae208aeba1856930575c54ab
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-20 18:37:13 +0100

    Delete blog app

--- dublin_bus/blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Blog(models.Model):
-    title = models.CharField(max_length=250)
-    body = models.TextField(max_length=500)
-
-    def __str__(self):
-        return self.title
--- dublin_bus/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BlogDetialView, BlogListView 
-
-urlpatterns = [
-    path('', BlogListView.as_view()),
-    path('<int:pk>/', BlogDetialView.as_view()),
-]
