6ab03ce3e3a2245791da0fdfdc219deb08bd81c5
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-24 12:56:48 +0100

    Update wordle modal

--- dublin_bus/frontend/src/components/wordle/modal.js
+++ dublin_bus/frontend/src/components/wordle/modal.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { CgArrowRight } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
 
 import { Modal, ModalHeader } from "../modal";
 import { PrimaryButton } from "../elements/button";
@@ -6,6 +8,8 @@ import { PrimaryButton } from "../elements/button";
 export function WordleModal(props) {
   const { turn, open, setOpen, solution, isCorrect, handleStartOver, ...rest } =
     props;
+
+  const navigate = useNavigate();
   const title = isCorrect ? "You got it!" : "You didn't get it!";
   const subtitle = isCorrect ? "" : `The correct answer was: ${solution}`;
 
@@ -13,7 +17,14 @@ export function WordleModal(props) {
     <Modal modalOpen={open} setModalOpen={setOpen}>
       <div {...rest}>
         <ModalHeader title={title} subtitle={subtitle} />
-        <div className='mt-5 sm:mt-6'>
+        <div
+          onClick={() => navigate("/")}
+          className='flex items-center justify-center text-primary-blue text-center cursor-pointer'
+        >
+          <p className='text-sm '>Back to journey</p>
+          <CgArrowRight />
+        </div>
+        <div className='flex items-center justify-center mt-5 sm:mt-6'>
           <PrimaryButton onClick={handleStartOver} type='large'>
             Play again
           </PrimaryButton>
