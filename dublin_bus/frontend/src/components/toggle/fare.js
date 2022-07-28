07d26831f367c73442b399a9e51921c0a9c18dcc
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-28 16:40:56 +0100

    Add fare to localstorage

--- dublin_bus/frontend/src/components/toggle/fare.js
+++ dublin_bus/frontend/src/components/toggle/fare.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoIosMan } from "react-icons/io";
 import { MdChildFriendly } from "react-icons/md";
 
@@ -7,18 +7,37 @@ import { Toggle } from ".";
 export function FareToggle() {
   const [isAdult, setIsAdult] = useState(true);
 
-  const handleToggle = () => {
-    //TODO code here for request
-    setIsAdult(!isAdult);
+  useEffect(() => {
+    const type = window.localStorage.getItem("fare");
+    console.log("type", type);
+    if (!type) {
+      window.localStorage.setItem("fare", "adult");
+      setIsAdult(true);
+    } else if (type === "adult") {
+      setIsAdult(true);
+    } else {
+      setIsAdult(false);
+    }
+  }, []);
+
+  const handleToggle = async () => {
+    const type = window.localStorage.getItem("fare");
+    if (!type || type === "adult") {
+      await window.localStorage.setItem("fare", "child");
+      setIsAdult(false);
+    } else {
+      await window.localStorage.setItem("fare", "adult");
+      setIsAdult(true);
+    }
   };
   return (
     <Toggle
       check={isAdult}
-      iconOne={IoIosMan}
+      iconOne={MdChildFriendly}
       fillOne={"#000000"}
       fillTwo={"#000000"}
       callback={handleToggle}
-      iconTwo={MdChildFriendly}
+      iconTwo={IoIosMan}
     />
   );
 }
