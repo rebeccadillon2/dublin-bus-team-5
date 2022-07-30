7fe3a178b333270a50bd7c4d0dae500f0e40b8e8
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-07-30 20:14:11 +0100

    Update form styling

--- dublin_bus/frontend/src/components/elements/form/date.js
+++ dublin_bus/frontend/src/components/elements/form/date.js
@@ -3,11 +3,12 @@ import TextField from "@mui/material/TextField";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { useTheme } from "../../../hooks";
+import { useTheme, useWindowSize } from "../../../hooks";
 
 export function BasicDateTimePicker({ time, setTime }) {
   const today = new Date();
   const [isDarkMode] = useTheme();
+  const [width] = useWindowSize();
   const theme = isDarkMode
     ? {
         backgroundColor: "#0C0C0D",
@@ -18,17 +19,21 @@ export function BasicDateTimePicker({ time, setTime }) {
         color: "#969ba5",
       };
 
+  const widthStyle =
+    width < 768
+      ? { width: "18rem", maxWidth: "18rem", minWidth: "18rem" }
+      : { width: "320px", maxWidth: "320px", minWidth: "320px" };
+
   const style = {
     maxHeight: "40px",
     padding: 0,
     paddingRight: "10px",
-    maxWidth: "320px",
-    minWidth: "320px",
     borderRadius: "10px",
     fontFamily: "Manrope",
     letterSpacing: "0.02rem",
     fontWeight: "500",
     ...theme,
+    ...widthStyle,
   };
 
   return (
