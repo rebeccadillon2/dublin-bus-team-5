484f9d7ae4111f62d0a0d48f4f429ad645882821
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-08-01 17:03:06 +0100

    Add login tests

--- dublin_bus/jwt_auth/tests/test_setup.py
+++ dublin_bus/jwt_auth/tests/test_setup.py
@@ -1,19 +1,30 @@
 from rest_framework.test import APITestCase
-from django.urls import reverse
 
 class TestSetUp(APITestCase):
     def setUp(self):
-        self.register_url=reverse('register')
-        self.register_url=reverse('login')
+        self.valid_reigster_data = {
+            "email": "eoin25@email.com",
+            "username":  "eoin25@email.com",
+            "password": "pass",
+            "password_confirmation": "pass"
+            }
 
-        self.user_data = {
-            "username": "a@email.com",
-            "email": "a@email.com",
-            "password": "a@email.com",
-            "passwordConfirmation": "a@email.com",
-            "profileImage":
-                        "https://res.cloudinary.com/dn11uqgux/image/upload/v1631736676/sei_project_3_studio_images/Screenshot_2021-09-15_at_21.09.58_vd6hdq.png",
-  };
+        self.invalid_register_data = {
+            "email": "eoin25@email.com",
+            "username":  "eoin25@email.com",
+            "password": "pass",
+            "password_confirmation": "passmatch"
+            }
+
+        self.valid_login_data = {
+            "email":  "eoin25@email.com",
+            "password": "pass",
+            }
+
+        self.invalid_login_data = {
+            "email":  "eoin25@email.com",
+            "password": "passt",
+            }
         
         return super().setUp()
 
--- dublin_bus/jwt_auth/tests/test_views.py
+++ dublin_bus/jwt_auth/tests/test_views.py
@@ -1,26 +1,33 @@
 from .test_setup import TestSetUp
+from ..models import User
 
 class TestViews(TestSetUp):    
-    def test_user_can_register(self):
-        data = {
- 	        "email": "eoin25@email.com",
- 	        "username":  "eoin25@email.com",
- 	        "password": "pass",
- 	        "password_confirmation": "pass"
-            }
-        res = self.client.post("/api/auth/register/", data, format='json')
+    def test_register_sucess(self): 
+        res = self.client.post("/api/auth/register/", self.valid_reigster_data, format='json')
         self.assertEqual(res.status_code, 201)
     
-    def test_user_cannot_register_no_data(self):
+
+    def test_register_fail_no_data(self):
         res = self.client.post("/api/auth/register/")
         self.assertEqual(res.status_code, 422)
 
-    def test_user_cannot_register_non_matching_password(self):
-        data = {
-            "email": "eoin25@email.com",
-            "username":  "eoin25@email.com",
-            "password": "pass",
-            "password_confirmation": "passmatch"
-            }
-        res = self.client.post("/api/auth/register/", data, format='json')
+
+    def test_register_fail_non_matching_password(self):
+        res = self.client.post("/api/auth/register/", self.invalid_register_data, format='json')
         self.assertEqual(res.status_code, 422)
+
+
+    def test_login_fail_non_existing_email_password_combination(self):
+        response = self.client.post("/api/auth/register/", self.valid_reigster_data, format='json')
+        res = self.client.post("/api/auth/login/", self.invalid_login_data, format='json')
+        user = User.objects.get(email="eoin25@email.com")
+        user.save()
+        self.assertEqual(res.status_code, 403)
+
+
+    def test_login_success(self):
+        response = self.client.post("/api/auth/register/", self.valid_reigster_data, format='json')
+        user = User.objects.get(email="eoin25@email.com")
+        user.save()
+        res = self.client.post("/api/auth/login/", self.valid_login_data, format='json')
+        self.assertEqual(res.status_code, 200)
