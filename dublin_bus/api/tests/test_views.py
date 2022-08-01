d0ee52a92fca08643d3e48d3393ca6fddb060a62
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-08-01 20:02:31 +0100

    Add ML tests

--- dublin_bus/api/tests/test_views.py
+++ dublin_bus/api/tests/test_views.py
@@ -21,3 +21,37 @@ class TestViews(TestSetUp):
         self.assertEqual(response.status_code, 200)
 
 
+    def test_all_get_ml_predict_least_data(self):
+        data = {
+            "headSign": "Saint Pappin's Road - Blackthorn Drive (Blackthorn Road)", 
+            "routeShortName": "11",
+            "humidity": 84,
+            "wind": 9.77,
+            "seconds": 71220,
+            "day": 0,
+            "month": 8,
+            "routeHeadSign": "St Pappin's Rd",
+            "numStops": 18
+            }
+        response = self.client.get('/api/bus/get-ml-predict/', data, format='json')
+        difference = abs(18-response.data)
+        self.assertGreaterEqual(7, difference)
+
+
+    def test_all_get_ml_predict_most_data(self):
+        data = {
+            "headSign": "Outside Heuston Train Station - Ballywaltrim", 
+            "routeShortName": "145",
+            "humidity": 84,
+            "wind": 9.77,
+            "seconds": 71220,
+            "day": 0,
+            "month": 8,
+            "routeHeadSign": "Heuston Station",
+            "numStops": 15
+            }
+        response = self.client.get('/api/bus/get-ml-predict/', data, format='json')
+        difference = abs(15-response.data)
+        self.assertGreaterEqual(7, difference)
+
+
