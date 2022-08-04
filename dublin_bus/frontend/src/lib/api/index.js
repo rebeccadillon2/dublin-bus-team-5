e808d1b93124f23478e2a6090a5e4dd90802688f
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-08-04 12:15:27 +0100

    Add new url

--- dublin_bus/frontend/src/lib/api/index.js
+++ dublin_bus/frontend/src/lib/api/index.js
@@ -1,7 +1,7 @@
 // const devUrl = `http://127.0.0.1:8000`;
 // const prodUrl = `http://127.0.0.1:8000`;
-const devUrl = `http://54.194.174.129`;
-const prodUrl = `http://54.194.174.129`;
+const devUrl = `http://34.245.27.75`;
+const prodUrl = `http://34.245.27.75`;
 export const baseUrl = process.env.NODE_ENV === "production" ? prodUrl : devUrl;
 
 export const headers = () => {
